dbc654db56878f6b698eb2b873dbf545e0ee3d86
Gorkem Ercan <gorkem.ercan@gmail.com>
2020-04-22 18:44:03 -0400

    Move tests to top level
    Movest the tests out of src. Adds a couple of basic tests.

---
+++ test/suite/extension.test.ts
@@ -0,0 +1,15 @@
+import * as assert from 'assert';
+
+import * as vscode from 'vscode';
+
+suite('Knative extension', () => {
+
+	test('Extension should be present', () => {
+		assert.ok(vscode.extensions.getExtension('redhat.vscode-knative'));
+	});
+
+	test('should activate',  async () => {
+		await vscode.extensions.getExtension('redhat.vscode-knative').activate();
+	});
+
+});
