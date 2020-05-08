8e03d22f32f1d7d741ee335056f813bac203adc2
Gorkem Ercan <gorkem.ercan@gmail.com>
2020-05-08 17:37:29 -0400

    simplified serviceDataProvider
    Simplify the serviceDataProvider and removes the treeModel.
    Adds tests for no service and single service

--- test/suite/tree/serviceDataProvider.test.ts
+++ test/suite/tree/serviceDataProvider.test.ts
@@ -1,27 +1,45 @@
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
 import * as sinon from 'sinon';
-import {ServiceDataProvider} from '../../../src/tree/serviceDataProvider';
+import { ServiceDataProvider } from '../../../src/tree/serviceDataProvider';
+import * as singleServiceData from './singleServiceServiceList.json';
 
-const {expect} = chai;
+const { expect } = chai;
 chai.use(sinonChai);
 
-suite('ServiceDataProvider ', () => {
+suite('ServiceDataProvider', () => {
   const sandbox = sinon.createSandbox();
   const serviceDataProvider = new ServiceDataProvider();
 
-  test('should return the No Services node when KN execute returns "No Services found"', async () => {
-    sandbox.stub(serviceDataProvider.knExecutor, "execute").resolves({error: null, stdout: "No services found."});
-    const result = await serviceDataProvider.getChildren();
-    expect(result).to.have.lengthOf(1);
-    expect(result[0].description).equals("No Service Found");
-    expect(result[0].label).equals("No Service Found");
-    expect(result[0].getName()).equals("No Service Found");
+  teardown(()=>{
+    sandbox.restore();
+  });
+  suite('No Services', () => {
+
+    test('getChildren should return the No Services node when KN execute returns "No Services found"', async () => {
+      sandbox
+        .stub(serviceDataProvider.knExecutor, 'execute')
+        .resolves({ error: undefined, stdout: 'No services found.' });
+      const result = await serviceDataProvider.getChildren();
+      expect(result).to.have.lengthOf(1);
+      expect(result[0].description).equals('No Service Found');
+      expect(result[0].label).equals('No Service Found');
+      expect(result[0].getName()).equals('No Service Found');
+    });
   });
 
-
-  teardown(()=>{
-      sandbox.restore();
+suite('Single service', ()=>{
+    test('getChildren should return single node', async () => {
+      sandbox
+        .stub(serviceDataProvider.knExecutor, 'execute')
+        .resolves({ error: undefined, stdout: JSON.stringify(singleServiceData) });
+      const result = await serviceDataProvider.getChildren();
+      expect(result).to.have.lengthOf(1);
+      expect(result[0].description).equals('greeter');
+      expect(result[0].label).equals('greeter');
+      expect(result[0].getName()).equals('greeter');
+      expect(result[0].tooltip).equals('Service: greeter');
+    });
   });
 
 });
