ba4a052c09d3e1837ad347c2de8b5c6df1372b53
joshuawilson <javajoshw@gmail.com>
2020-05-22 23:28:26 -0400

    fix(service): add all details for service and revision models

--- src/knative/revision.ts
+++ src/knative/revision.ts
@@ -6,16 +6,122 @@
 import { KnativeItem } from './knativeItem';
 
 export class Revision extends KnativeItem {
-  name: string;
-
-  service: string;
+  constructor(public name: string, public service: string, public details?: Items) {
+    super();
+  }
 
   status: boolean;
 
-  static toRevision(value: any): Revision {
-    const revision = new Revision();
-    revision.name = value.metadata.name;
-    revision.service = value.metadata.ownerReferences[0].name;
+  static toRevision(value: Items): Revision {
+    const revision = new Revision(value.metadata.name, value.metadata.ownerReferences[0].name, value);
     return revision;
   }
+}
+export interface JSONRevision {
+  apiVersion: string;
+  items?: Items[] | null;
+  kind: string;
+}
+export interface Items {
+  apiVersion: string;
+  kind: string;
+  metadata: Metadata;
+  spec: Spec;
+  status: Status;
+}
+export interface Metadata {
+  annotations: Annotations;
+  creationTimestamp: string;
+  generateName?: string | null;
+  generation: number;
+  labels: Labels;
+  managedFields?: ManagedFields[] | null;
+  name: string;
+  namespace: string;
+  ownerReferences?: OwnerReferencesEntity[] | null;
+  resourceVersion: string;
+  selfLink: string;
+  uid: string;
+}
+export interface Annotations {
+  'image.openshift.io/triggers'?: string | null;
+  'serving.knative.dev/creator': string;
+  'serving.knative.dev/lastPinned': string;
+  'client.knative.dev/user-image'?: string | null;
+}
+export interface Labels {
+  'app.kubernetes.io/component'?: string | null;
+  'app.kubernetes.io/instance'?: string | null;
+  'app.kubernetes.io/name'?: string | null;
+  'app.kubernetes.io/part-of'?: string | null;
+  'app.openshift.io/runtime'?: string | null;
+  'app.openshift.io/runtime-namespace'?: string | null;
+  'app.openshift.io/runtime-version'?: string | null;
+  'serving.knative.dev/configuration': string;
+  'serving.knative.dev/configurationGeneration': string;
+  'serving.knative.dev/route'?: string | null;
+  'serving.knative.dev/service': string;
+}
+export interface ManagedFields {
+  apiVersion: string;
+  fieldsType: string;
+  fieldsV1: FieldsV1;
+  manager: string;
+  operation: string;
+  time: string;
+}
+export interface FieldsV1 {
+  'f:metadata'?: {} | null;
+  'f:spec'?: {} | null;
+  'f:status'?: {} | null;
+}
+export interface OwnerReferencesEntity {
+  apiVersion: string;
+  blockOwnerDeletion: boolean;
+  controller: boolean;
+  kind: string;
+  name: string;
+  uid: string;
+}
+export interface Spec {
+  containerConcurrency: number;
+  containers?: Containers[] | null;
+  timeoutSeconds: number;
+}
+export interface Containers {
+  image: string;
+  imagePullPolicy?: string | null;
+  name: string;
+  ports?: Ports[] | null;
+  readinessProbe: ReadinessProbe;
+  resources: Resources;
+}
+export interface Ports {
+  containerPort: number;
+}
+export interface ReadinessProbe {
+  successThreshold: number;
+  tcpSocket: TcpSocket;
+}
+export interface TcpSocket {
+  port: number;
+}
+export interface Resources {
+  limits: {};
+  requests: {};
+}
+export interface Status {
+  conditions?: Conditions[] | null;
+  imageDigest: string;
+  logUrl: string;
+  observedGeneration: number;
+  serviceName: string;
+}
+export interface Conditions {
+  lastTransitionTime: string;
+  message?: string | null;
+  reason?: string | null;
+  severity?: string | null;
+  status: string;
+  type: string;
 }
--- src/knative/service.ts
+++ src/knative/service.ts
@@ -18,16 +18,145 @@ export interface CreateService {
 }
 
 export class Service extends KnativeItem implements CreateService {
-  constructor(public name: string, public image: string) {
+
+  constructor(
+    public name: string,
+    public image: string,
+    public details?: Items
+  ) {
     super();
   }
 
+  env?: Map<string, string>;
+
+  port?: number;
+
+  force?: boolean;
+
+  annotation?: Map<string, boolean>;
+
+  label?: Map<string, string>;
+
+  namespace?: string;
+
   revisions: Revision[];
 
-  static toService(value: any): Service {
-    const service = new Service(value.metadata.name, value.status.url);
-    // service.name = value.metadata.name;
-    // service.image = value.status.url;
+  static toService(value: Items): Service {
+    const service = new Service(
+      value.metadata.name,
+      value.status.url,
+      value
+    );
     return service;
   }
+}
+
+export interface JSONService {
+  apiVersion?: string;
+  items?: Items[] | null;
+  kind?: string;
+}
+export interface Items {
+  apiVersion: string;
+  kind: string;
+  metadata: Metadata;
+  spec: Spec;
+  status: Status;
+}
+export interface Metadata {
+  annotations: Annotations;
+  creationTimestamp: string;
+  generation: number;
+  labels?: Labels | null;
+  managedFields?: ManagedFields[] | null;
+  name: string;
+  namespace: string;
+  resourceVersion: string;
+  selfLink: string;
+  uid: string;
+}
+export interface Annotations {
+  'openshift.io/generated-by'?: string | null;
+  'serving.knative.dev/creator': string;
+  'serving.knative.dev/lastModifier': string;
+  'client.knative.dev/user-image'?: string | null;
+}
+export interface Spec {
+  template: Template;
+  traffic?: Traffic[] | null;
+}
+export interface Template {
+  metadata: Metadata;
+  spec: TemplateSpec;
+}
+
+export interface Labels {
+  'app.kubernetes.io/component': string;
+  'app.kubernetes.io/instance': string;
+  'app.kubernetes.io/name': string;
+  'app.kubernetes.io/part-of': string;
+  'app.openshift.io/runtime': string;
+  'app.openshift.io/runtime-namespace': string;
+  'app.openshift.io/runtime-version': string;
+}
+export interface ManagedFields {
+  apiVersion: string;
+  fieldsType: string;
+  fieldsV1: FieldsV1;
+  manager: string;
+  operation: string;
+  time: string;
+}
+export interface FieldsV1 {
+  'f:metadata'?: {} | null;
+  'f:spec'?: {} | null;
+  'f:status'?: {} | null;
+}
+export interface TemplateSpec {
+  containerConcurrency: number;
+  containers?: Containers[] | null;
+  timeoutSeconds: number;
+}
+export interface Containers {
+  image: string;
+  name: string;
+  ports?: Ports[] | null;
+  readinessProbe: ReadinessProbe;
+  resources: Resources;
+}
+export interface Ports {
+  containerPort: number;
+}
+export interface ReadinessProbe {
+  successThreshold: number;
+  tcpSocket: TcpSocket;
+}
+export interface TcpSocket {
+  port: number;
+}
+export interface Resources {
+  limits: {};
+  requests: {};
+}
+export interface Traffic {
+  latestRevision: boolean;
+  percent: number;
+  revisionName: string;
+}
+export interface Status {
+  address: Address;
+  conditions?: Conditions[] | null;
+  latestCreatedRevisionName: string;
+  latestReadyRevisionName: string;
+  observedGeneration: number;
+  traffic?: Traffic[] | null;
+  url: string;
+}
+export interface Address {
+  url: string;
+}
+export interface Conditions {
+  lastTransitionTime: string;
+  status: string;
+  type: string;
 }
