98273b23a14bd73cee3f6163235104d734ae38d9
joshuawilson <javajoshw@gmail.com>
2020-05-29 17:19:19 -0400

    fix(test): add tests for service Data Provider

--- test/tree/serviceDataProvider.test.ts
+++ test/tree/serviceDataProvider.test.ts
@@ -1,6 +1,8 @@
+import * as vscode from 'vscode';
 import * as chai from 'chai';
 import * as sinonChai from 'sinon-chai';
 import * as sinon from 'sinon';
+import * as referee from '@sinonjs/referee';
 import { ServiceDataProvider } from '../../src/tree/serviceDataProvider';
 import * as singleServiceData from './singleServiceServiceList.json';
 import * as singleServiceRevisionData from './singleServiceRevisionList.json';
@@ -9,6 +11,9 @@ import { ContextType } from '../../src/kn/config';
 import { KnativeItem } from '../../src/knative/knativeItem';
 import { Service } from '../../src/knative/service';
 
+// import rewire = require('rewire');
+
+const { assert } = referee;
 const { expect } = chai;
 chai.use(sinonChai);
 
@@ -19,6 +24,27 @@ suite('ServiceDataProvider', () => {
   teardown(() => {
     sandbox.restore();
   });
+
+  // TODO: figure out how to test an event that is fired.
+  suite('Refresh', () => {
+    test('should fire the tree data change event', () => {
+      // const api = rewire('../../src/tree/serviceDataProvider');
+      // const onDidChangeTreeDataEmitter = api.__get__('onDidChangeTreeDataEmitter');
+      // const spy = sandbox.spy(onDidChangeTreeDataEmitter, 'fire');
+      serviceDataProvider.refresh();
+      // assert(spy.called);
+    });
+  });
+
+  suite('Getting a Tree Item', () => {
+    test('should return the specific tree element requested', async () => {
+      const parentKnativeItem: KnativeItem = new Service('greeter', 'quay.io/rhdevelopers/knative-tutorial-greeter:quarkus');
+      const parent: KnativeTreeItem = new KnativeTreeItem(null, parentKnativeItem, 'greeter', ContextType.SERVICE, 0, null, null);
+      const item: vscode.TreeItem = await serviceDataProvider.getTreeItem(parent);
+      assert.equals(item, parent);
+    });
+  });
+
   suite('No Services', () => {
     test('getChildren should return the No Services node when KN execute returns "No Services found"', async () => {
       sandbox.stub(serviceDataProvider.knExecutor, 'execute').resolves({ error: undefined, stdout: 'No services found.' });
@@ -57,6 +83,28 @@ suite('ServiceDataProvider', () => {
       expect(result[0].label).equals('greeter-btrnq-1');
       expect(result[0].getName()).equals('greeter-btrnq-1');
       expect(result[0].tooltip).equals('Revision: greeter-btrnq-1');
+    });
+  });
+
+  suite('Getting a Parent Item', () => {
+    test('should return null for a Service', () => {
+      const parentKnativeItem: KnativeItem = new Service('greeter', 'quay.io/rhdevelopers/knative-tutorial-greeter:quarkus');
+      const parent: KnativeTreeItem = new KnativeTreeItem(null, parentKnativeItem, 'greeter', ContextType.SERVICE, 0, null, null);
+      const item: KnativeTreeItem = serviceDataProvider.getParent(parent);
+      assert.equals(item, null);
+    });
+  });
+
+  suite('Getting a Parent Item', () => {
+    test('should return the Service of the Revision', async () => {
+      sandbox
+        .stub(serviceDataProvider.knExecutor, 'execute')
+        .resolves({ error: undefined, stdout: JSON.stringify(singleServiceRevisionData) });
+      const parentKnativeItem: KnativeItem = new Service('greeter', 'quay.io/rhdevelopers/knative-tutorial-greeter:quarkus');
+      const parent: KnativeTreeItem = new KnativeTreeItem(null, parentKnativeItem, 'greeter', ContextType.SERVICE, 0, null, null);
+      const result = await serviceDataProvider.getRevisions(parent);
+      const item: KnativeTreeItem = serviceDataProvider.getParent(result[0]);
+      assert.equals(item, parent);
     });
   });
 });
