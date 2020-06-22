a4119f3bb7bbf1e9c36db19aa28c1f7e2f9ee511
Ondrej Dockal <odockal@redhat.com>
2020-06-22 15:51:40 +0200

    Adding Filters utility unit tests
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

---
+++ test/util/filters.test.ts
@@ -0,0 +1,48 @@
+import { expect } from 'chai';
+import { Filters } from '../../src/util/filters';
+
+import sinon = require('sinon');
+
+suite('Filters utility class', () => {
+  const sandbox = sinon.createSandbox();
+
+  teardown(() => {
+    sandbox.restore();
+  });
+
+  test('should replace token string in given text', () => {
+    const spy = sandbox.spy(Filters, 'filterToken');
+    expect(Filters.filterToken('some string and given --token=xxx, should result in filtered string')).to.include(
+      '--token **********',
+    );
+    expect(spy.calledOnce);
+  });
+
+  test('should not replace similar token-like expression string in given text', () => {
+    expect(
+      Filters.filterToken('some string and given token=xxx or --token xxx or -- token=xxx, should not result in filtered string'),
+    ).not.to.include('--token **********');
+  });
+
+  test('should replace password string in given text', () => {
+    const spy = sandbox.spy(Filters, 'filterToken');
+    expect(
+      Filters.filterPassword(`some string and given password -p 'MySecretT3xt?*', should result in filtered string`),
+    ).to.include('-p **********');
+    expect(spy.calledOnce);
+  });
+
+  test('should not replace similar password-like string or parameter annotation in given text', () => {
+    expect(
+      Filters.filterPassword(
+        `some string and given parameter --p 'not exacltyPass' or --p='again not a password' neither -p=someValue`,
+      ),
+    ).to.include('-p **********');
+  });
+
+  test('should return same falsy object if not full string', () => {
+    expect(Filters.filterToken('')).to.equal('');
+    expect(Filters.filterToken(null)).to.equal(null);
+    expect(Filters.filterToken(undefined)).to.equal(undefined);
+  });
+});
