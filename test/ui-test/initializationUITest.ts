0bdb686cc904e69e67c54df226a7152423e24a95
Ondrej Dockal <odockal@redhat.com>
2020-09-14 17:43:25 +0200

    Refactor UI Test suites (#82)
    * Refactor UI Test suites
       - test suites for internal and external CI systems
       - add ui test for action items
       - udpate to github actions workflow to include base UI test suite
       - update Jenkinsfile
    
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

---
+++ test/ui-test/initializationUITest.ts
@@ -0,0 +1,43 @@
+import { expect, assert } from 'chai';
+import { ActivityBar, VSBrowser, NotificationType, WebDriver } from 'vscode-extension-tester';
+import { KNativeConstants } from './common/constants';
+import { getNotifications, cleanUpNotifications } from './common/testUtils';
+
+/**
+ * @author Ondrej Dockal <odockal@redhat.com>
+ */
+export function knativeInitializationUITest(): void {
+  let driver: WebDriver;
+
+  before(async () => {
+    driver = VSBrowser.instance.driver;
+    await cleanUpNotifications();
+  });
+
+  describe('Knative view', () => {
+    it('should be ready for usage, requires access to the cluster', async function context() {
+      this.timeout(10000);
+      const view = new ActivityBar().getViewControl(KNativeConstants.KNATIVE_EXTENSION_NAME);
+      const sideBar = await view.openView();
+      // check that no notification error appeared
+      try {
+        await driver.wait(async () => {
+          const notifs = await getNotifications(NotificationType.Error, NotificationType.Warning);
+          return notifs === null || notifs === undefined || notifs.length === 0;
+        }, 5000);
+      } catch (error) {
+        assert.fail(`Error notification appeared during cluster loading`);
+      }
+      const content = sideBar.getContent();
+      expect(await content.getText()).to.equal(KNativeConstants.NO_SERVICE_FOUND);
+      const sections = await content.getSections();
+      expect(sections.length).to.equal(1);
+      const section = sections[0];
+      expect(await section.getTitle()).to.equal(KNativeConstants.KNATIVE_EXTENSION_NAME);
+      const items = await sections[0].getVisibleItems();
+      expect(items.length).to.equal(1);
+      const item = items[0];
+      expect(await item.getText()).to.equal(KNativeConstants.NO_SERVICE_FOUND);
+    });
+  });
+}
