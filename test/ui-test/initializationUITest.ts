0216702b08548f6f306f7260f2b85e9ca11a266a
Ondrej Dockal <odockal@redhat.com>
2021-01-27 23:10:01 +0100

    Adapt UI tests for serving and eventing sections
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- test/ui-test/initializationUITest.ts
+++ test/ui-test/initializationUITest.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import { expect, assert } from 'chai';
 import { ActivityBar, VSBrowser, NotificationType, WebDriver } from 'vscode-extension-tester';
 import { KNativeConstants } from './common/constants';
@@ -10,14 +11,13 @@ export function knativeInitializationUITest(): void {
   let driver: WebDriver;
 
   before(async function setup() {
-    this.timeout(10000);
     await cleanUpNotifications();
     driver = VSBrowser.instance.driver;
   });
 
   describe('Knative view', () => {
     it('should be ready for usage, requires access to the cluster', async function context() {
-      this.timeout(10000);
+      this.timeout(25000);
       const view = new ActivityBar().getViewControl(KNativeConstants.KNATIVE_EXTENSION_NAME);
       const sideBar = await view.openView();
       // check that no notification error appeared
@@ -29,17 +29,26 @@ export function knativeInitializationUITest(): void {
       } catch (error) {
         assert.fail(`Error notification appeared during cluster loading`);
       }
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      driver.wait(async () => !(await sideBar.getContent().hasProgress()), 3000);
-      expect(await sideBar.getContent().getText()).to.equal(KNativeConstants.NO_SERVICE_FOUND);
-      const sections = await sideBar.getContent().getSections();
-      expect(sections.length).to.equal(1);
-      const section = sections[0];
-      expect(await section.getTitle()).to.equal(KNativeConstants.KNATIVE_EXTENSION_NAME);
-      const items = await sections[0].getVisibleItems();
-      expect(items.length).to.equal(1);
-      const item = items[0];
-      expect(await item.getText()).to.equal(KNativeConstants.NO_SERVICE_FOUND);
+      await driver.wait(async () => !(await sideBar.getContent().hasProgress()), 3000);
+      const servingSection = await sideBar.getContent().getSection(KNativeConstants.SECTION_SERVING);
+      if (!(await servingSection.isExpanded())) {
+        await servingSection.expand();
+      }
+      console.log(await servingSection.getText());
+      console.log(await servingSection.getTitle());
+      await driver.wait(async () => (await servingSection.getVisibleItems()).length > 0, 10000);
+      console.log(await Promise.all((await servingSection.getVisibleItems()).map((item) => item.getText())));
+      const items = await servingSection.getVisibleItems();
+      expect(await items[0].getText()).to.equal(KNativeConstants.NO_SERVICE_FOUND);
+      const eventingSection = await sideBar.getContent().getSection(KNativeConstants.SECTION_EVENTING);
+      if (!(await eventingSection.isExpanded())) {
+        await eventingSection.expand();
+      }
+      await driver.wait(async () => (await eventingSection.getVisibleItems()).length > 0, 5000);
+      const brokersTree = await eventingSection.getVisibleItems();
+      console.log(await Promise.all(brokersTree.map((item) => item.getText())));
+      expect(brokersTree.length).to.be.greaterThan(0);
+      expect(brokersTree).to.has.members(['Brokens', 'Channels', 'Sources', 'Subscriptions', 'Triggers']);
     });
   });
 }
