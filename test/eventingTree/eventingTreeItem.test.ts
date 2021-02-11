a78a3170e25210d760fa6f7553235e867c9aab6f
joshuawilson <javajoshw@gmail.com>
2021-02-11 13:14:10 -0500

    fix(test): add tests for eventingTreeItem

--- test/eventingTree/eventingTreeItem.test.ts
+++ test/eventingTree/eventingTreeItem.test.ts
@@ -6,7 +6,10 @@ import * as sinon from 'sinon';
 import * as sinonChai from 'sinon-chai';
 import * as referee from '@sinonjs/referee';
 import * as brokerData from './broker.json';
+import * as channelData from './channel.json';
 import * as sourceData from './source.json';
+import * as subscriptionData from './subscription.json';
+import * as triggerData from './trigger.json';
 import { EventingContextType } from '../../src/cli/config';
 import { EventingTreeItem } from '../../src/eventingTree/eventingTreeItem';
 import { Broker } from '../../src/knative/broker';
@@ -14,6 +17,9 @@ import { GenericSource } from '../../src/knative/genericSource';
 import { APIServerSource } from '../../src/knative/apiServerSource';
 import { PingSource } from '../../src/knative/pingSource';
 import { BindingSource } from '../../src/knative/bindingSource';
+import { Channel } from '../../src/knative/channel';
+import { Subscription } from '../../src/knative/subscription';
+import { Trigger } from '../../src/knative/trigger';
 
 const { assert } = referee;
 chai.use(sinonChai);
@@ -87,6 +93,22 @@ suite('EventingTreeItem', () => {
     null,
   );
 
+  // Channels
+  const testChannel0: Channel = new Channel(
+    'example-channel0',
+    'Channels',
+    'InMemoryChannel',
+    JSON.parse(JSON.stringify(channelData.items[0])),
+  );
+  // Channel Tree Items
+  const testChannel0TreeItem: EventingTreeItem = new EventingTreeItem(
+    eventingFolderNodes[1],
+    testChannel0,
+    { label: 'example-channel0' },
+    EventingContextType.CHANNEL,
+    vscode.TreeItemCollapsibleState.Expanded,
+  );
+
   // API Server Source
   const testSource0: APIServerSource = new APIServerSource(
     'example-source0',
@@ -157,6 +179,70 @@ suite('EventingTreeItem', () => {
     null,
   );
 
+  // Subscriptions
+  const testSubscription0: Subscription = new Subscription(
+    'example-subscription0',
+    'Subscriptions',
+    'example-channel0',
+    'aaa',
+    undefined,
+    undefined,
+    JSON.parse(JSON.stringify(subscriptionData.items[5])),
+  );
+  // Subscription Tree Items
+  const testSubscription0TreeItem: EventingTreeItem = new EventingTreeItem(
+    eventingFolderNodes[3],
+    testSubscription0,
+    { label: 'example-subscription0' },
+    EventingContextType.SUBSCRIPTION,
+    vscode.TreeItemCollapsibleState.Expanded,
+  );
+
+  // Triggers
+  const filters = new Map();
+  filters.set('name', 'dev.knative.bar');
+  filters.set('type', 'dev.knative.foo');
+  const testTrigger0: Trigger = new Trigger(
+    'example-trigger0',
+    'Triggers',
+    'example-broker0',
+    filters,
+    'aaa',
+    JSON.parse(JSON.stringify(triggerData.items[0])),
+  );
+  const testTrigger4: Trigger = new Trigger(
+    'example-trigger4',
+    'Triggers',
+    'example-broker1',
+    filters,
+    'https://event.receiver.uri/',
+    JSON.parse(JSON.stringify(triggerData.items[2])),
+  );
+  // Trigger Tree Items
+  const testTrigger0TreeItem: EventingTreeItem = new EventingTreeItem(
+    eventingFolderNodes[4],
+    testTrigger0,
+    { label: 'example-trigger0' },
+    EventingContextType.TRIGGER,
+    vscode.TreeItemCollapsibleState.Expanded,
+  );
+  const testTrigger4TreeItem: EventingTreeItem = new EventingTreeItem(
+    eventingFolderNodes[4],
+    testTrigger4,
+    { label: 'example-trigger4' },
+    EventingContextType.TRIGGER,
+    vscode.TreeItemCollapsibleState.Expanded,
+  );
+
+  // URI
+  const testURIForTrigger4TreeItem: EventingTreeItem = new EventingTreeItem(
+    testTrigger4TreeItem,
+    null,
+    { label: 'Sink - https://event.receiver.uri/' },
+    EventingContextType.URI,
+    vscode.TreeItemCollapsibleState.None,
+  );
+
   test('should get the icon path', () => {
     const revPath = testBroker0TreeItem.iconPath;
     const localDir = __dirname;
@@ -210,43 +296,72 @@ suite('EventingTreeItem', () => {
     assert.equals(tested, []);
   });
 
-  test('should get the children of Broker', () => {
+  test('should get the children of the Broker Folder', () => {
     const tested = eventingFolderNodes[0].getChildren();
     assert.equals(tested, []);
   });
 
-  test('should get the children of channel', () => {
+  test('should get the children of a Broker', () => {
+    const tested = testBroker0TreeItem.getChildren();
+    assert.equals(tested, []);
+  });
+
+  test('should get the children of the Channel Folder', () => {
     const tested = eventingFolderNodes[1].getChildren();
     assert.equals(tested, []);
   });
 
-  test('should get the children of source', () => {
-    const tested = testSource2TreeItem.getChildren();
+  test('should get the children of a Channel', () => {
+    const tested = testChannel0TreeItem.getChildren();
     assert.equals(tested, []);
   });
 
-  test('should get the children of source_apiserver', () => {
+  test('should get the children of the Source Folder', () => {
+    const tested = eventingFolderNodes[2].getChildren();
+    assert.equals(tested, []);
+  });
+
+  test('should get the children of a source_apiserver', () => {
     const tested = testSource0TreeItem.getChildren();
     assert.equals(tested, []);
   });
 
-  test('should get the children of source_binding', () => {
-    const tested = testSource3TreeItem.getChildren();
-    assert.equals(tested, []);
-  });
-
-  test('should get the children of source_ping', () => {
+  test('should get the children of a source_ping', () => {
     const tested = testSource1TreeItem.getChildren();
     assert.equals(tested, []);
   });
 
-  test('should get the children of subscription', () => {
+  test('should get the children of a source_generic', () => {
+    const tested = testSource2TreeItem.getChildren();
+    assert.equals(tested, []);
+  });
+
+  test('should get the children of a source_binding', () => {
+    const tested = testSource3TreeItem.getChildren();
+    assert.equals(tested, []);
+  });
+
+  test('should get the children of the Subscription Folder', () => {
     const tested = eventingFolderNodes[3].getChildren();
     assert.equals(tested, []);
   });
 
-  test('should get the children of trigger', () => {
+  test('should get the children of a Subscription', () => {
+    const tested = testSubscription0TreeItem.getChildren();
+    assert.equals(tested, []);
+  });
+
+  test('should get the children of the Trigger Folder', () => {
     const tested = eventingFolderNodes[4].getChildren();
+    assert.equals(tested, []);
+  });
+  test('should get the children of a Trigger', () => {
+    const tested = testTrigger0TreeItem.getChildren();
+    assert.equals(tested, []);
+  });
+
+  test('should get the children of a URI', () => {
+    const tested = testURIForTrigger4TreeItem.getChildren();
     assert.equals(tested, []);
   });
 });
