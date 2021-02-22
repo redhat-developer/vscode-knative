eacbff7a24e1a921473a8e3f8abea5f926bbaa0e
Ondrej Dockal <odockal@redhat.com>
2021-02-22 15:53:47 +0100

    Move and place some UI tests
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- test/ui-test/initializationUITest.ts
+++ test/ui-test/initializationUITest.ts
@@ -1,9 +1,7 @@
-/* eslint-disable no-return-await */
-/* eslint-disable no-console */
 import { expect, assert } from 'chai';
 import { ActivityBar, VSBrowser, NotificationType, WebDriver } from 'vscode-extension-tester';
 import { KNativeConstants } from './common/constants';
-import { findNotification, getNotifications, safeNotificationExists } from './common/testUtils';
+import { getNotifications, cleanUpNotifications } from './common/testUtils';
 
 /**
  * @author Ondrej Dockal <odockal@redhat.com>
@@ -11,55 +9,15 @@ import { findNotification, getNotifications, safeNotificationExists } from './co
 export function knativeInitializationUITest(): void {
   let driver: WebDriver;
 
-  before(function setup() {
-    // await cleanUpNotifications();
-    process.env.KUBECONFIG = '/home/odockal/kubeconfig';
+  before(async function setup() {
+    await cleanUpNotifications();
+    // process.env.KUBECONFIG = '/home/odockal/kubeconfig';
     driver = VSBrowser.instance.driver;
-  });
-
-  describe('Knative extension', () => {
-    it('requires Knative CLI tool Notification to pop up after Knative view is opened', async function context() {
-      this.timeout(10000);
-      const view = new ActivityBar().getViewControl(KNativeConstants.KNATIVE_EXTENSION_NAME);
-      const sideBar = await view.openView();
-      expect(await sideBar.isDisplayed()).to.equal(true);
-      await driver.wait(async () => {
-        return await safeNotificationExists('Cannot find Knative CLI');
-      }, 5000);
-    });
-    it('allows to download missing kn cli using notification', async function context() {
-      this.timeout(90000);
-      const notification = await driver.wait(async () => {
-        return findNotification('Cannot find Knative CLI');
-      }, 3000);
-      const actions = await notification.getActions();
-      //   const action = await asyncFilter(actions, async (item) => {
-      //     const text = await item.getText();
-      //     return text.indexOf('Download') > 0;
-      //   });
-      const actionsTexts = await Promise.all(actions.map(async (item) => item.getText()));
-      const downloadActionText = actionsTexts.find((item) => {
-        console.log(`going over: ${item}`);
-        const index = item.indexOf('Download');
-        console.log(`${index}`);
-        return item.includes('Download') ? item : undefined;
-      });
-      console.log(downloadActionText);
-      await notification.takeAction(downloadActionText);
-      await driver.wait(async () => {
-        return findNotification('Downloading Knative CLI');
-      }, 3000);
-      await driver.wait(async () => {
-        const exists = await safeNotificationExists('Downloading Knative CLI');
-        console.log(`Notification exists: ${exists}`);
-        return !exists;
-      }, 80000);
-    });
   });
 
   describe('Knative view', () => {
     it('should be ready for usage, requires access to the cluster', async function context() {
-      this.timeout(60000);
+      this.timeout(90000);
       const view = new ActivityBar().getViewControl(KNativeConstants.KNATIVE_EXTENSION_NAME);
       const sideBar = await view.openView();
       // check that no notification error appeared
@@ -76,7 +34,7 @@ export function knativeInitializationUITest(): void {
       if (!(await servingSection.isExpanded())) {
         await servingSection.expand();
       }
-      await driver.wait(async () => (await servingSection.getVisibleItems()).length > 0, 15000);
+      await driver.wait(async () => (await servingSection.getVisibleItems()).length > 0, 30000);
       const items = await servingSection.getVisibleItems();
       expect(await items[0].getText()).to.equal(KNativeConstants.NO_SERVICE_FOUND);
       const eventingSection = await sideBar.getContent().getSection(KNativeConstants.SECTION_EVENTING);
