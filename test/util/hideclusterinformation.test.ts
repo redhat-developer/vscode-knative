99a65c612af9a45148ca4df3ef202c04ffce2c58
Sudhir Verma <sudhirvvermaa@gmail.com>
2021-12-13 17:29:45 +0530

    Added more test

---
+++ test/util/hideclusterinformation.test.ts
@@ -0,0 +1,55 @@
+/*-----------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE file in the project root for license information.
+ *-----------------------------------------------------------------------------------------------*/
+
+import * as chai from 'chai';
+import * as sinonChai from 'sinon-chai';
+import { hideClusterInfo, hideIPAddress, hidePath } from '../../src/util/hideclusterinformation';
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+suite('Hide user information', () => {
+  const ipAddress =
+    'undefinedUnable to connect to the server: dial tcp 192.168.1.240:6443: connect: no route to host 192.168.1.240:6443 2001:0000:3238:DFE1:63::FEFB 2001:0:3238:DFE1:63::FEFB';
+
+  const userPath = `undefinedUnable to connect to the server: error executing access token command "C:\\!Users\\test\\AppData\\Local\\test-code\\installer\\test-cloud-sdk\\bin\\test.cmd config config-helper --format=json": err=exec: "C:\\Users\test\\AppData\\Local\\test-code\\installer\\test-cloud-sdk\\₭ǝ℞ᖭ\\₭ǝ℞ᖭ.cmd": file does not exist output= stderr=
+  "/₭℞ᖭ/apple/₭℞ᖭ/"      "./test/test" "test/test2" "test" `;
+
+  const hideClusterData =
+    'undefinedUnable to connect to the server: dial tcp 192.168.1.240:6443: connect: no route to host 192.168.1.240:6443 2001:0000:3238:DFE1:63::FEFB  https://test.com     connect to the server: dial tcp: lookup https://test.com:10.10.20.30: no such host';
+
+  test('hide cluster information', () => {
+    const result = hideClusterInfo(hideClusterData);
+    expect(result).deep.equals(
+      'undefinedUnable to connect to the server: dial tcp IP Address :6443: connect: no route to host IP Address :6443 IPV6 Address   cluster info      connect to the server: dial tcp: lookup cluster info:  no such host',
+    );
+  });
+
+  test('Hide local user path', () => {
+    const result = hidePath(userPath);
+    expect(result).deep
+      .equals(`undefinedUnable to connect to the server: error executing access token command "local path": err=exec: "local path": file does not exist output= stderr=
+  "local path"      "local path" "local path" "test" `);
+  });
+
+  test('return string if local user path not found', () => {
+    const message = 'undefinedUnable to connect to the server: error executing access token command';
+    const result = hidePath(message);
+    expect(result).deep.equals(message);
+  });
+
+  test('Hide user ipAddress', () => {
+    const result = hideIPAddress(ipAddress);
+    expect(result).deep.equals(
+      'undefinedUnable to connect to the server: dial tcp IP Address :6443: connect: no route to host IP Address :6443 IPV6 Address  IPV6 Address ',
+    );
+  });
+
+  test('return string if user ipAddress not found', () => {
+    const message = 'undefinedUnable to connect to the server: dial tcp connect: no route to host';
+    const result = hideIPAddress(message);
+    expect(result).deep.equals(message);
+  });
+});
