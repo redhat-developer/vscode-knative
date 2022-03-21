06ba2ea7e3da5ffc8751e143a92d3313edf4af7c
Sudhir Verma <sudhirvvermaa@gmail.com>
2022-03-21 17:51:28 +0530

    implementing invoke

--- src/functions/function-command/invoke-function.ts
+++ src/functions/function-command/invoke-function.ts
@@ -29,7 +29,8 @@ import {
 } from './invoke-function-def';
 // eslint-disable-next-line import/no-cycle
 import { contextGlobalState } from '../../extension';
-import { createValidationItem } from '../validate-item';
+import { createValidationItem, inputFieldValidation, pathValidation, selectLocationValidation } from '../validate-item';
+import { invokeFunctionID } from '../webview-id';
 
 export const invokeItemMap = new Map<string, boolean>();
 
@@ -122,8 +123,106 @@ export const def: WizardDefinition = {
       hideWizardPageHeader: true,
       fields: localInvokeTextDef,
       // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-      validator: () => {
-        const items = [];
+      validator: (parameters: ParametersType) => {
+        const items: ValidatorResponseItem[] = [];
+        if (parameters.invokeId !== undefined && !Number.isNaN(parameters.invokeId) && !parameters.invokeId?.trim()) {
+          inputFieldValidation(
+            {
+              value: parameters.invokeId,
+              id: invokeFunctionID.invoke_ID,
+              message: 'Provide id',
+            },
+            items,
+          );
+        }
+        if (
+          parameters.invokeNamespace !== undefined &&
+          !Number.isNaN(parameters.invokeNamespace) &&
+          !parameters.invokeNamespace?.trim()
+        ) {
+          inputFieldValidation(
+            {
+              value: parameters.invokeNamespace,
+              id: invokeFunctionID.invoke_namespace,
+              message: 'Provide namespace',
+            },
+            items,
+          );
+        }
+        if (parameters.invokePath !== undefined && !Number.isNaN(parameters.invokePath)) {
+          selectLocationValidation(
+            {
+              value: parameters.invokePath,
+              id: invokeFunctionID.invoke_path,
+              message: 'Provide path.',
+            },
+            items,
+          );
+        }
+        if (
+          parameters.invokeContextType !== undefined &&
+          !Number.isNaN(parameters.invokeContextType) &&
+          !parameters.invokeContextType?.trim()
+        ) {
+          inputFieldValidation(
+            {
+              value: parameters.invokeContextType,
+              id: invokeFunctionID.invoke_context_type,
+              message: 'Provide context type',
+            },
+            items,
+          );
+        }
+        if (parameters.invokeSource !== undefined && !Number.isNaN(parameters.invokeSource) && !parameters.invokeSource?.trim()) {
+          inputFieldValidation(
+            {
+              value: parameters.invokeSource,
+              id: invokeFunctionID.invoke_source,
+              message: 'Provide source',
+            },
+            items,
+          );
+        }
+        if (parameters.invokeType !== undefined && !Number.isNaN(parameters.invokeType) && !parameters.invokeType?.trim()) {
+          inputFieldValidation(
+            {
+              value: parameters.invokeType,
+              id: invokeFunctionID.invoke_type,
+              message: 'Provide type',
+            },
+            items,
+          );
+        }
+        if (
+          parameters.invokeDataText !== undefined &&
+          !Number.isNaN(parameters.invokeDataText) &&
+          !parameters.invokeDataText?.trim() &&
+          parameters.invokeDataModeText === 'Text'
+        ) {
+          inputFieldValidation(
+            {
+              value: parameters.invokeDataText,
+              id: invokeFunctionID.invoke_data_text,
+              message: 'Provide data',
+            },
+            items,
+          );
+        }
+        if (
+          parameters.invokeDataFile !== undefined &&
+          !Number.isNaN(parameters.invokeDataFile) &&
+          !parameters.invokeDataFile?.trim() &&
+          parameters.invokeDataModeFile === 'File'
+        ) {
+          inputFieldValidation(
+            {
+              value: parameters.invokeDataFile,
+              id: invokeFunctionID.invoke_data_file,
+              message: 'Select file',
+            },
+            items,
+          );
+        }
         return { items };
       },
     },
@@ -195,7 +294,23 @@ export const def: WizardDefinition = {
         // eslint-disable-next-line @typescript-eslint/no-use-before-define
         invokeFunction(contextGlobalState);
       }
-      return true;
+      const validPath = pathValidation.get('path_validation');
+      return (
+        data.invokeId !== undefined &&
+        data.invokeId?.trim()?.length !== 0 &&
+        data.invokeContextType !== undefined &&
+        data.invokeContextType?.trim()?.length !== 0 &&
+        data.invokeSource !== undefined &&
+        data.invokeSource?.trim()?.length !== 0 &&
+        data.invokeType !== undefined &&
+        data.invokeType?.trim()?.length !== 0 &&
+        (data.invokeDataModeFile === 'File'
+          ? data.invokeDataModeFile !== undefined && data.invokeDataModeFile?.trim()?.length !== 0
+          : data.invokeDataModeText !== undefined && data.invokeDataModeText?.trim()?.length !== 0) &&
+        (data.invokeInstance === 'Local'
+          ? data.invokePath !== undefined && data.invokePath?.trim()?.length !== 0 && validPath
+          : data.invokeNamespace !== undefined && data.invokeNamespace?.trim()?.length !== 0)
+      );
     },
     performFinish(): null {
       return null;
