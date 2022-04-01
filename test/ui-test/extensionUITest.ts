a482014f24e202478302ec6461885afd12d5809b
Ondrej Dockal <odockal@redhat.com>
2022-04-01 11:51:27 +0200

    Update Github actions workflow, linux only for ui tests and arfiving artefacts (#210)
    Signed-off-by: Ondrej Dockal <odockal@redhat.com>

--- test/ui-test/extensionUITest.ts
+++ test/ui-test/extensionUITest.ts
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import { ActivityBar, ViewControl, SideBarView, WebDriver, VSBrowser, ModalDialog } from 'vscode-extension-tester';
 import { KNativeConstants } from './common/constants';
 import { cleanUpNotifications, findNotification, safeNotificationExists } from './common/testUtils';
+
 /**
  * @author Ondrej Dockal <odockal@redhat.com>
  */
@@ -41,8 +42,7 @@ export function extensionsUITest(clusterIsAvailable: boolean): void {
     });
 
     it('allows to download missing kn cli using notification', async function context() {
-      this.timeout(100000);
-      console.log('findNotification');
+      this.timeout(50000);
       const notification = await driver.wait(async () => findNotification('Cannot find Knative CLI'), 10000);
       console.log('getActions');
       const actions = await notification.getActions();
@@ -52,13 +52,25 @@ export function extensionsUITest(clusterIsAvailable: boolean): void {
       const downloadActionText = actionsTexts.find((item) => (item.includes('Download') ? item : undefined));
       console.log('takeAction');
       await notification.takeAction(downloadActionText);
-      console.log('findNotification Download Knative CLI');
-      await driver.wait(async () => findNotification('Downloading Knative CLI'), 3000);
-      console.log('safenotificationexists');
+      await driver.wait(async () => findNotification('Downloading Knative CLI'), 10000);
       await driver.wait(async () => {
         const exists = await safeNotificationExists('Downloading Knative CLI');
         return !exists;
-      }, 80000);
+      }, 30000);
+    });
+
+    it('allows to download missing kubectl binary using notification', async function context() {
+      this.timeout(50000);
+      const notification = await driver.wait(async () => findNotification('Cannot find Kubernetes CLI'), 10000);
+      const actions = await notification.getActions();
+      const actionsTexts = await Promise.all(actions.map(async (item) => item.getText()));
+      const downloadActionText = actionsTexts.find((item) => (item.includes('Download') ? item : undefined));
+      await notification.takeAction(downloadActionText);
+      await driver.wait(async () => findNotification('Downloading Kubernetes CLI'), 10000);
+      await driver.wait(async () => {
+        const exists = await safeNotificationExists('Downloading Kubernetes CLI');
+        return !exists;
+      }, 30000);
     });
 
     it('should contain Serving, Eventing sections and Function sections', async function context() {
