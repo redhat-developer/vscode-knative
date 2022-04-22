f76fb4232cace163f68652598888376c47b1cb5c
Sudhir Verma <sudhirvvermaa@gmail.com>
2022-04-22 13:59:28 +0530

    Fix detect kubectl command

--- src/check-cluster.ts
+++ src/check-cluster.ts
@@ -4,12 +4,12 @@
  *  Licensed under the MIT License. See LICENSE file in the project root for license information.
  *-----------------------------------------------------------------------------------------------*/
 
-import { executeCmdCli } from './cli/cmdCli';
+import { knExecutor } from './cli/execute';
 import { KubectlAPI } from './cli/kubectl-api';
 
 export async function checkOpenShiftCluster(): Promise<boolean> {
   try {
-    const result = await executeCmdCli.executeExec(KubectlAPI.checkOcpCluster());
+    const result = await knExecutor.execute(KubectlAPI.checkOcpCluster(), process.cwd(), false);
     if (result?.stdout?.trim()) {
       return true;
     }
