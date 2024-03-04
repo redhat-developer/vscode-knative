c1b63e07fa51f6ee34f8ea20f3761594dc792eaa
msivasubramaniaan <msivasub@redhat.com>
2024-03-04 14:49:17 +0530

    update install-vscode
    Signed-off-by: msivasubramaniaan <msivasub@redhat.com>

--- build/install-vscode.ts
+++ build/install-vscode.ts
@@ -6,20 +6,33 @@
 import { execSync } from 'child_process';
 import { platform } from 'os';
 import { dirname, join } from 'path';
+import path = require('path');
 import { downloadAndUnzipVSCode } from '@vscode/test-electron';
 
 downloadAndUnzipVSCode()
   .then((executable: string): void => {
+    // Install extensions that openshift-toolkit depends on
+    // eslint-disable-next-line global-require, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+    const packageJson = require('../package.json');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const extensionsToInstall = packageJson.extensionDependencies;
     let exe: string = executable;
     if (platform() === 'darwin') {
       exe = `'${join(exe.substring(0, exe.indexOf('.app') + 4), 'Contents', 'Resources', 'app', 'bin', 'code')}'`;
     } else {
       exe = join(dirname(exe), 'bin', 'code');
     }
-    const installMsg = execSync(`${exe} --install-extension ms-kubernetes-tools.vscode-kubernetes-tools`);
-    // eslint-disable-next-line no-console
-    console.log('Installing message', installMsg.toString());
-    execSync(`${exe} --install-extension redhat.vscode-yaml`);
+    const extensionRootPath = path.resolve(__dirname, '..', '..');
+    const vsCodeTest = path.resolve(path.join(extensionRootPath, '.vscode-test'));
+    const userDataDir = path.join(vsCodeTest, 'user-data');
+    const extDir = path.join(vsCodeTest, 'extensions');
+    // eslint-disable-next-line no-restricted-syntax
+    for (const extension of extensionsToInstall) {
+      // eslint-disable-next-line no-console
+      console.log('Installing extension: ', extension);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      execSync(`${exe} --install-extension ${extension} --user-data-dir ${userDataDir} --extensions-dir ${extDir}`);
+    }
   })
   // eslint-disable-next-line no-console, @typescript-eslint/restrict-template-expressions
   .catch((err) => console.log(`There was an error while downloading and unzipping, error = ${err}`));
